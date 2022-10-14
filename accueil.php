ee2a8d2fb238d454b13bac20425a6950060bfa94
Dhan-aurel <dboudoh@normandiewebschool.fr>
2022-10-14 15:31:49 +0200

    parte front finito
    ho cancelato tante cose per rendere il formulare simplice per il back

--- accueil.php
+++ accueil.php
@@ -1,3 +1,10 @@
+<?php
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -5,7 +12,6 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/accueil.css">
-    <link rel="stylesheet" href="css/link.css">
 	<title>Annuaire_nws</title>
     <link rel="icon" href="images/nws_logo.png">
 </head>
